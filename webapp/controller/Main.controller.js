23802a1e4ab183a9389ded6ce0f9f3601f977f1a
Cristiano Biral Jorge <cristianobiral@brq.com>
2023-03-30 13:35:42 -0300

    second class

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,15 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/library"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, library) {
         "use strict";
+
+        var urlHelper = library.URLHelper;
 
         return Controller.extend("barcodeproject.controller.Main", {
             onInit: function () {
 
+            },
+
+            onPressSeach: function() {
+                alert('Searching...');
+            },
+
+            onClickImage: function(oEvent) {
+                urlHelper.redirect(oEvent.getSource().getSrc(), true);
             }
         });
     });
